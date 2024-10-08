72ffb5c8b4135773ed7eda0a1a9386e7236638b4
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2024-10-08 16:45:29 +0300

    düzenleme-yapıldı

---
+++ needlist/src/App.jsx
@@ -0,0 +1,13 @@
+import { GirisSayfa } from "./components/GirisSayfa";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <GirisSayfa />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ needlist/src/components/GirisSayfa.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function GirisSayfa() {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Need List</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Password</Label>
+              <Input id="pasword" placeholder="***********" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button
+          variant="outline"
+          className="border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white"
+        >
+          Login
+        </Button>
+        <Button>Register</Button>
+      </CardFooter>
+    </Card>
+  );
+}
